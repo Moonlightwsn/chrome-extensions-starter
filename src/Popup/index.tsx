e7c4ab8c4425ca5a629bc6156e36c116db49cea4
Moonlightwsn <543456277@qq.com>
2024-11-12 10:12:33 +0800

    feat: PopupApp

--- src/Popup/index.tsx
+++ src/Popup/index.tsx
@@ -1,18 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+import reactLogo from "../assets/react.svg"
+import viteLogo from "/vite.svg"
+import "./index.css"
 
-function App() {
+function PopupApp() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
+        <a href="https://vite.dev" target="_blank" rel="noopener">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noopener">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
@@ -32,4 +32,4 @@ function App() {
   )
 }
 
-export default App
+export default PopupApp
