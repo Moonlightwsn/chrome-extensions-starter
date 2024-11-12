4ac25ad68ed2a8d0615b456e601a68f03851116c
Moonlightwsn <543456277@qq.com>
2024-11-12 14:38:07 +0800

    feat: Content App create shadow root

--- src/Content/index.tsx
+++ src/Content/index.tsx
@@ -1,24 +1,39 @@
 import React from "react"
-import ReactDOM from "react-dom/client"
-
 import packageJson from "~/package.json"
 import viteLogo from "~/public/vite.svg"
-import css from "./index.css?inline"
 
-const extensionName = packageJson.name || "CRX Demo"
-const contentRoot = document.createElement("div")
-contentRoot.id = `${extensionName}-content`
-document.body.append(contentRoot)
+import createShadowRoot from "./createShadowRoot"
+import styles from "./index.css?inline"
+
+const __DEV__ = process.env.NODE_ENV === "development"
+const extensionId = `${packageJson.name || "CRX Demo"}-content-root`
+const tagName = "div"
 
 const viteLogoUrl = chrome.runtime.getURL(viteLogo)
-ReactDOM.createRoot(contentRoot).render(
-  <React.StrictMode>
-    <style type="text/css">{css}</style>
+void (async () => {
+  const root = createShadowRoot(extensionId, tagName, {
+    style: __DEV__ ? "" : styles,
+    mode: __DEV__ ? "open" : "closed",
+  })
+
+  const LogoBox = () => (
     <div
-      className="fixed right-[24px] bottom-[24px] h-[48px] w-[48px] z-[1001] rounded-full overflow-hidden cursor-pointer"
-      title={extensionName}
+      className="fixed right-4 bottom-4 h-12 w-12 rounded-full overflow-hidden cursor-pointer"
+      title={extensionId}
     >
       <img className="w-full h-full" src={viteLogoUrl} alt="vite-logo" />
     </div>
-  </React.StrictMode>
-)
+  )
+
+  root.render(
+    <React.StrictMode>
+      <LogoBox />
+    </React.StrictMode>
+  )
+
+  if (__DEV__) {
+    await import("./index.css")
+    const styleElement = document.querySelector("[data-vite-dev-id]")!
+    root.shadowRoot.insertBefore(styleElement, root.shadowRoot.firstChild)
+  }
+})()
