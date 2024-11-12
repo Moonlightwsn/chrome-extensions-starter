670f4380dce06a06b2311a71b5e05b0753ff9931
Moonlightwsn <543456277@qq.com>
2024-11-12 11:46:09 +0800

    feat: show vite logo in Content App

--- src/Content/index.tsx
+++ src/Content/index.tsx
@@ -2,14 +2,19 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 
 import packageJson from "~/package.json"
+import viteLogo from "~/public/vite.svg"
+import "./index.css"
 
 const extensionName = packageJson.name || "CRX Demo"
 const contentRoot = document.createElement("div")
 contentRoot.id = `${extensionName}-content`
 document.body.append(contentRoot)
 
+const viteLogoUrl = chrome.runtime.getURL(viteLogo)
 ReactDOM.createRoot(contentRoot).render(
   <React.StrictMode>
-    <div>{extensionName}</div>
+    <div className="vite-logo-wrap" title={extensionName}>
+      <img src={viteLogoUrl} alt="vite-logo" />
+    </div>
   </React.StrictMode>
 )
