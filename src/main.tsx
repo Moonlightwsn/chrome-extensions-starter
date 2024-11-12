e5dfd7ed2b5b536452323bb6420c11cbc3f04df3
Moonlightwsn <543456277@qq.com>
2024-11-12 10:17:01 +0800

    chore: vite alias config

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
-import "./index.css"
-import PopupApp from "./Popup"
+import "@/index.css"
+import PopupApp from "@/Popup"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from "path"
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import { crx } from "@crxjs/vite-plugin"
@@ -6,4 +7,9 @@ import manifest from "./manifest.config"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), crx({ manifest })],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
